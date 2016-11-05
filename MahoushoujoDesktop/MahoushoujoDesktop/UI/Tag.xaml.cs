dd8ee7d1f9ad3089b7458f7e596b8d8e605f1f31
Dwscdv3 <dwscdv3@hotmail.com>
2016-11-05 18:51:09 +0800

    tag

---
+++ MahoushoujoDesktop/MahoushoujoDesktop/UI/Tag.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System . Collections . Generic;
+using System . Linq;
+using System . Text;
+using System . Threading . Tasks;
+using System . Windows;
+using System . Windows . Controls;
+using System . Windows . Data;
+using System . Windows . Documents;
+using System . Windows . Input;
+using System . Windows . Media;
+using System . Windows . Media . Imaging;
+using System . Windows . Navigation;
+using System . Windows . Shapes;
+
+namespace MahoushoujoDesktop . UI
+{
+    /// <summary>
+    /// Tag.xaml 的交互逻辑
+    /// </summary>
+    public partial class Tag : UserControl
+    {
+        public event RoutedEventHandler TagClick;
+
+
+
+        public string Text
+        {
+            get { return (string) GetValue ( TextProperty ); }
+            set { SetValue ( TextProperty , value ); }
+        }
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty . Register ( "Text" , typeof ( string ) , typeof ( Tag ) ,
+                new PropertyMetadata (
+                    "" ,
+                    ( d , e ) =>
+                      {
+                          ( d as Tag ) . textTag . Text = (string) e . NewValue;
+                      } )
+                );
+        public new Brush Background
+        {
+            get { return (Brush) GetValue ( BackgroundProperty ); }
+            set { SetValue ( BackgroundProperty , value ); }
+        }
+        public static readonly new DependencyProperty BackgroundProperty =
+            DependencyProperty . Register ( "Background" , typeof ( Brush ) , typeof ( Tag ) ,
+                new PropertyMetadata (
+                    null ,
+                    ( d , e ) =>
+                    {
+                        ( d as Tag ) . rectMain . Fill = (Brush) e . NewValue;
+                    } )
+                );
+
+
+
+        public Tag ()
+        {
+            InitializeComponent ();
+        }
+
+        private void buttonTag_Click ( object sender , RoutedEventArgs e )
+        {
+            
+        }
+
+        private void buttonDelete_Click ( object sender , RoutedEventArgs e )
+        {
+
+        }
+    }
+}
